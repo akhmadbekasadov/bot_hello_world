1de2416f31c7235f84d5dfb903a8a239038cf57e
Асадов Ахмадбек <akhmadbek.asadov@inside.glowbyte.com>
2026-04-07 02:22:01 +0300

    update config

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # Дата начала соревнований (День 1)
-START_DATE = "2026-04-08"
+START_DATE = "2026-01-12"
 
 # Часовой пояс
 TIMEZONE = "Europe/Moscow"
@@ -13,4 +13,4 @@ POST_MINUTE = 0
 # {total} — всего дней
 # {date}  — дата в формате ДД.ММ.ГГГГ
 TOTAL_DAYS = 365
-MESSAGE_TEMPLATE = "День {day} / {total}\n{date}\nКидаем сюда >>>"
+MESSAGE_TEMPLATE = "День {day} / {total}\n{date}\nКидаем сюда 👇"
